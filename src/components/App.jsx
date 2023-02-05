8b05e501b8904ce911775e2d1c1b4ee4cdf92dd3
Tennis Kumar C <70009826+tennis-kumar@users.noreply.github.com>
2023-02-05 20:15:48 +0530

    Renamd Login.jsx to Form.jsx, add conditnal rendr

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
 
 var isLoggedIn = false;
+var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      {isLoggedIn ? <h1> Hello </h1> : <Login />}
+      {isLoggedIn ? <h1> Hello </h1> : <Form isRegistered={userIsRegistered} />}
     </div>
   );
 }
